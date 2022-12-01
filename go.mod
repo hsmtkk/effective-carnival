8aad3fc62a5c7251d7bd041320255d394b0d7cc4
hsmtkk <hsmtkk@gmail.com>
2022-12-01 10:12:33 +0900

    up

---
+++ go.mod
@@ -0,0 +1,12 @@
+module cdk.tf/go/stack
+
+go 1.16
+
+require github.com/aws/constructs-go/constructs/v10 v10.1.161
+
+require github.com/hashicorp/terraform-cdk-go/cdktf v0.14.1
+
+require (
+	github.com/aws/jsii-runtime-go v1.71.0
+	github.com/cdktf/cdktf-provider-google-go/google/v4 v4.0.1
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/cdktf/cdktf-provider-google-go/google/v4/artifactregistryrepository"
+	"github.com/cdktf/cdktf-provider-google-go/google/v4/cloudbuildtrigger"
+	"github.com/cdktf/cdktf-provider-google-go/google/v4/provider"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+
+const (
+	owner      = "hsmtkk"
+	project    = "effective-carnival"
+	region     = "asia-northeast1"
+	repository = "effective-carnival"
+)
+
+func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
+	stack := cdktf.NewTerraformStack(scope, &id)
+
+	provider.NewGoogleProvider(stack, jsii.String("google"), &provider.GoogleProviderConfig{
+		Project: jsii.String(project),
+	})
+
+	artifactregistryrepository.NewArtifactRegistryRepository(stack, jsii.String("artifact_registry"), &artifactregistryrepository.ArtifactRegistryRepositoryConfig{
+		Format:       jsii.String("docker"),
+		Location:     jsii.String(region),
+		RepositoryId: jsii.String("registry"),
+	})
+
+	cloudbuildtrigger.NewCloudbuildTrigger(stack, jsii.String("cloud_build_trigger"), &cloudbuildtrigger.CloudbuildTriggerConfig{
+		Filename: jsii.String("cloudbuild.yaml"),
+		Github: &cloudbuildtrigger.CloudbuildTriggerGithub{
+			Name:  jsii.String(repository),
+			Owner: jsii.String(owner),
+			Push: &cloudbuildtrigger.CloudbuildTriggerGithubPush{
+				Branch: jsii.String("main"),
+			},
+		},
+	})
+
+	return stack
+}
+
+func main() {
+	app := cdktf.NewApp(nil)
+
+	stack := NewMyStack(app, "effective-carnival")
+	cdktf.NewCloudBackend(stack, &cdktf.CloudBackendProps{
+		Hostname:     jsii.String("app.terraform.io"),
+		Organization: jsii.String("hsmtkkdefault"),
+		Workspaces:   cdktf.NewNamedCloudWorkspace(jsii.String("effective-carnival")),
+	})
+
+	app.Synth()
+}
